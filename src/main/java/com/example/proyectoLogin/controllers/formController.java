51419cfddb9465a3f94e4b7dc489b157dd2afcef
omar11developer <omarmenjivar083@gmail.com>
2023-09-16 16:50:27 -0600

    Terminamos controller

--- src/main/java/com/example/proyectoLogin/controllers/formController.java
+++ src/main/java/com/example/proyectoLogin/controllers/formController.java
@@ -5,15 +5,19 @@ import com.example.proyectoLogin.editors.PaisPropertyEditor;
 import com.example.proyectoLogin.editors.RolesEditor;
 import com.example.proyectoLogin.models.domain.Pais;
 import com.example.proyectoLogin.models.domain.Role;
+import com.example.proyectoLogin.models.domain.Usuario;
 import com.example.proyectoLogin.services.PaisService;
 import com.example.proyectoLogin.services.RoleService;
 import com.example.proyectoLogin.validation.UsuarioValidador;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -67,6 +71,56 @@ public class formController {
     public Map<String, String> listaRolesMap(){
         Map<String, String> roles = new HashMap<>();
         roles.put("ROLE_ADMIN","Administrador");
+        roles.put("ROLE_USER","Usuario");
+        roles.put("ROLE_MODERATOR","Moderador");
+
+        return roles;
+    }
+    @ModelAttribute("paises")
+    public List<String> paises(){
+        return Arrays.asList("España","Mexico","Chile","Argentina","Peru","Colombia","Venezuela");
+    }
+    @ModelAttribute("paisMap")
+    public Map<String, String> paisesMap(){
+        Map<String, String> paises = new HashMap<>();
+        paises.put("ES","España");
+        paises.put("MX","Mexico");
+        paises.put("CL","Chile");
+        paises.put("AR","Argentina");
+        paises.put("PE","Peru");
+        paises.put("CO","Colombia");
+        paises.put("VE","Venezuela");
+        return paises;
     }
 
+    @GetMapping("/form")
+    public String form(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Jhon");
+        usuario.setApellido("Doe");
+        usuario.setIndentificador("123.456.789-K");
+        usuario.setHabilitar(true);
+        usuario.setValorSecreto("Algun valor secreto ****");
+        usuario.setPais(new Pais(3,"CL","Chile"));
+        usuario.setRoles(Arrays.asList(new Role(2,"Usuario","ROLE_USER")));
+        model.addAttribute("titulo", "Formulario usuario");
+        model.addAttribute("usuario", usuario);
+        return "form";
+    }
+    @PostMapping("/form")
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("titulo", "Resultado form");
+        }
+        return "redirect:/ver";
+    }
+    @GetMapping("/ver")
+    public String ver(@SessionAttribute(name="usuario",required =false) Usuario usuario, Model model, SessionStatus status){
+        if(usuario == null){
+            return "redirect:/form";
+        }
+        model.addAttribute("titulo","Resultado form");
+        status.setComplete();
+        return "resultado";
+    }
 }
